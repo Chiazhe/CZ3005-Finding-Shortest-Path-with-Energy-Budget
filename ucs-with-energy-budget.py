b7c1c3e154b2d77a090cd32564240ee882b7ec8b
Chiazhe <chiazhe7@gmail.com>
2021-10-14 01:32:42 +0800

    part 1 and 2 done

--- ucs-with-energy-budget.py
+++ ucs-with-energy-budget.py
@@ -1,8 +1,11 @@
 import json
+import sys
 from queue import PriorityQueue
 
 
-def uniform_cost_search(source, destination):
+def uniform_cost_search(source, destination, energyLimit):
+    minimumDistance = 99999999999999999
+    answer = []
     if source not in graph.keys():
         print("Source node: "+source+" is not found in the graph!")
         return []
@@ -14,16 +17,17 @@ def uniform_cost_search(source, destination):
     # Used to check if a node is visited or not
     visited = set()
 
-    # Used to store the route in an increasing order of cost
+    # Used to store the route in an increasing order of distance
     pQueue = PriorityQueue()
-    pQueue.put((0, [source]))
+    pQueue.put((0, 0, [source]))
 
     while pQueue:
         # No route exist between them in the case
         if pQueue.empty():
-            print('Route not found between '+source+' and '+destination)
-            return 0
-        cost, route = pQueue.get()
+            print('Route not found between '+source+' and ' +
+                  destination+" at energy budget " + str(energyLimit))
+            return answer
+        distance, energy, route = pQueue.get()
         # currentNode is the current node we are at
         currentNode = route[len(route)-1]
 
@@ -31,25 +35,42 @@ def uniform_cost_search(source, destination):
         if currentNode not in visited:
             # Add current node into visited set
             visited.add(currentNode)
+
+            # Check if the path is larger than the minimum path as of now
+            if distance > minimumDistance:
+                continue
+
+            # Check if energy is more than the limit
+            if energy > energyLimit:
+                continue
+
             # Destination is Reached
             if currentNode == destination:
-                route.append(cost)
-                return route
+                minimumDistance = distance
+                route.append(distance)
+                route.append(energy)
+                answer.append(route)
+                # return route
 
             # Get the neighbors of current node
             neighbors = graph[currentNode]
             for n in neighbors:
                 if n not in visited:
-                    # Total Cost to this neighbor
-                    t_cost = costData[currentNode+","+n] + cost
+                    # Total energy to this neighbor
+                    t_energy = energyData[currentNode+","+n] + energy
+
+                    # Total Distance to this neighbor
+                    t_distance = distData[currentNode+","+n] + distance
 
                     # Array of the previous route before reaching this neighbor
                     temp = route[:]
                     # Add in this neighbor into the route
                     temp.append(n)
 
-                    # Add the cost and route for this neighbor for further explore later
-                    pQueue.put((t_cost, temp))
+                    # Add the distance, energy and route for this neighbor for further explore later
+                    pQueue.put((t_distance, t_energy, temp))
+
+    return answer
 
 
 # main function
@@ -61,7 +82,8 @@ if __name__ == '__main__':
         distData = json.load(f2)
 
     with open('Cost.json') as f3:
-        costData = json.load(f3)
+        energyData = json.load(f3)
 
-    answer = uniform_cost_search('3', '1254')
+    answer = uniform_cost_search('1', '65412', 9999999999999)
+    print(len(answer))
     print(answer)
